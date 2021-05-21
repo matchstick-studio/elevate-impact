f6208fce43029f6052a461f87f5e510639cc2e7d
Matchstick Studio <support@matchstick.ug>
2021-05-21 23:04:15 +0300

    Added more districts and data

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/kollinsayz/ck0sjmwa337rk1cqv0xrr8sxe',
+    style: 'mapbox://styles/kollinsayz/ckoypfzer2qzi17mh891t1iyz/?fresh=true',
     accessToken: 'pk.eyJ1Ijoia29sbGluc2F5eiIsImEiOiJjaWVoNzJxcTcwMDlvc3JtMXBtNmg4aWl6In0.T8YV7TzgBTTWQT19_pjceQ',
     showMarkers: true,
     markerColor: '#3FB1CE',
@@ -19,27 +19,15 @@ var config = {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
                 center: [32.68233, 1.08554],
-                zoom: 6.33,
+                zoom: 6.90,
                 pitch: 0,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [
-                // {
-                //     layer: '',
-                //     opacity: '',
-                //     duration: ''
-                // }
-            ],
-            onChapterExit: [
-                // {
-                //     layer: '',
-                //     opacity: '',
-                //     duration: ''
-                // }
-            ]
+            onChapterEnter: [],
+            onChapterExit: []
         },
         {
             id: 'mukono',
@@ -75,14 +63,14 @@ var config = {
             onChapterEnter: [
                 {
                     layer: 'mukono-0sd7q8',
-                    opacity: 0.6,
-                    duration: 5000
+                    opacity: 0.9,
+                    duration: 1000
                 }
             ],
             onChapterExit: [
                 {
                     layer: 'mukono-0sd7q8',
-                    opacity: 0
+                    opacity: 0.6
                 }
             ]
         },
@@ -109,10 +97,207 @@ var config = {
                 
             },
             location: {
-                center: [-77.020636, 38.886900],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: -43.2
+                center: [32.89055, 0.98984],
+                zoom: 9.46,
+                pitch: 45,
+                bearing: 0.0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {
+                    layer: 'kayunga-4ffse1',
+                    opacity: 0.9,
+                    duration: 1000
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'kayunga-4ffse1',
+                    opacity: 0.6,
+                }
+            ]
+        },
+        {
+            id: 'bugiri',
+            alignment: 'right',
+            hidden: false,
+            title: 'Bugiri',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            vt: {
+                
+            },
+            tt: {
+                
+            },
+            pd: {
+                schools: 30,
+                pupils: 203783,
+                teachers: 413,
+                community: 1800
+            },
+            location: {
+                center: [33.79544, 0.51890],
+                zoom: 9.46,
+                pitch: 45,
+                bearing: 0.0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'bugweri',
+            alignment: 'right',
+            hidden: false,
+            title: 'Bugweri',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            vt: {
+                
+            },
+            tt: {
+                
+            },
+            pd: {
+                schools: 30,
+                pupils: 10028,
+                teachers: 308,
+                community: 1800
+            },
+            location: {
+                center: [33.61330, 0.62178],
+                zoom: 10.90,
+                pitch: 38.00,
+                bearing: -39.20
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'iganga',
+            alignment: 'left',
+            hidden: false,
+            title: 'Iganga',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            vt: {
+                
+            },
+            tt: {
+                
+            },
+            pd: {
+                schools: 30,
+                pupils: 18172,
+                teachers: 373,
+                community: 1800
+            },
+            location: {
+                center: [33.47071, 0.72228],
+                zoom: 10.85,
+                pitch: 48.50,
+                bearing: 74.40
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'kaliro',
+            alignment: 'left',
+            hidden: false,
+            title: 'Kaliro',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            vt: {
+                
+            },
+            tt: {
+                
+            },
+            pd: {
+                schools: 30,
+                pupils: 22908,
+                teachers: 475,
+                community: 1800
+            },
+            location: {
+                center: [33.48579, 1.07996],
+                zoom: 10.66,
+                pitch: 38.50,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'luuka',
+            alignment: 'right',
+            hidden: false,
+            title: 'Luuka',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            vt: {
+                
+            },
+            tt: {
+                
+            },
+            pd: {
+                schools: 30,
+                pupils: 24746,
+                teachers: 479,
+                community: 1800
+            },
+            location: {
+                center: [33.32641, 0.81837],
+                zoom: 9.37,
+                pitch: 38.00,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'mayuge',
+            alignment: 'right',
+            hidden: false,
+            title: 'Mayuge',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            vt: {
+                
+            },
+            tt: {
+                
+            },
+            pd: {
+                schools: 30,
+                pupils: 25190,
+                teachers: 457,
+                community: 1800
+            },
+            location: {
+                center: [33.48642, -0.24818],
+                zoom: 8.73,
+                pitch: 38.00,
+                bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
